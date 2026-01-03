d1f95ab6c66f55ebf01682fae808736d301574f4
SSanjayRamasamy <er.sanjayramasamy@gmail.com>
2026-01-03 21:56:12 +0530

    Fixed README paths for simulation images

---
+++ 2_Combinational_Circuits/parity_Checker/Odd/Parity_Checker_Odd_tb.v
@@ -0,0 +1,21 @@
+module Parity_Checker_Odd_tb ();
+
+localparam dataSize=10; 
+reg [dataSize-1:0] data_in;
+reg parity_bit;
+wire error_flag;
+
+
+Parity_Checker_Odd #(dataSize) UUT (data_in,parity_bit,error_flag);
+
+initial begin
+
+    repeat(10) begin
+        data_in=$urandom;
+        parity_bit=$urandom;
+        #10;
+    end
+    $finish;
+end
+
+endmodule
